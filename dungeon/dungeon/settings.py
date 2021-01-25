51542f9def49efb9f69ba66487ef5153bd5bc67d
LeoKnox <LeoKnox@users.noreply.github.com>
2021-01-24 21:33:23 -0800

    Add files via upload

--- dungeon/dungeon/settings.py
+++ dungeon/dungeon/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'dungeon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'adventure_django',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
