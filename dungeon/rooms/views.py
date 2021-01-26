f870fa252f370ae1d6bba591480194d69c61a12f
LeoKnox <LeoKnox@users.noreply.github.com>
2021-01-25 21:26:37 -0800

    Add files via upload

--- dungeon/rooms/views.py
+++ dungeon/rooms/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("This is the first room")
+
+def display_room(request, room_id):
+    return HttpResponse("This is room %s." % room_id)
+
+def edit_room(request, room_id):
+    return HttpResponse("Edit this room %s!" % room_id)
