69b794031ec2cefe2aa12d3db7d01765d051a6ef
LeoKnox <LeoKnox@users.noreply.github.com>
2021-01-25 21:33:16 -0800

    Add files via upload

--- dungeon/rooms/urls.py
+++ dungeon/rooms/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:room_id>/', views.display_room, name='display_room'),
+    path('<int:room_id>/edit/', views.edit_room, name='edit_room')
 ]
