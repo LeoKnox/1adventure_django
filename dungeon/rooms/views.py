3655fa7535237e1a6677e36388201d04f564d56d
LeoKnox <LeoKnox@users.noreply.github.com>
2021-01-28 20:50:44 -0800

    Add files via upload

--- dungeon/rooms/views.py
+++ dungeon/rooms/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 from .models import Room, Door
 
@@ -9,9 +9,12 @@ def index(request):
     return render(request, 'rooms/index.html', context)
 
 def display_room(request, room_id):
-    room = Room.objects.get(pk = room_id)
-    doors = Door.objects.get(room = room.name)
-    context =  { 'room': room, 'door': doors }
+    try:
+        room = Room.objects.get(pk = room_id)
+        doors = Door.objects.get(room = room.name)
+        context =  { 'room': room, 'door': doors }
+    except room.DoesNotExist:
+        raise Http404("Room does not exist")
     return HttpResponse(request, 'rooms/single_room.html', context)
 
 def edit_room(request, room_id):
